296458426c21791d0176b7454cd9463921c3b4b2
Davit-Kunchulia <148901201+Davit-Kunchulia@users.noreply.github.com>
2024-01-29 19:08:48 +0400

    more than half

---
+++ index.js
@@ -0,0 +1,74 @@
+// 1
+let array = [5,25,89,120,36]
+
+array.push("javascript","python")
+array.unshift("html","css")
+
+console.log(array);
+
+array.pop()
+array.shift()
+
+console.log(array);
+
+
+// 3
+let array2 =[1, 2, 3, 4, 5]
+
+let array3 =['a', 'b', 'c']
+
+let spl = array2.splice(0,3)
+let spl2 = array2.splice(0,2)
+
+console.log(spl.concat(array3,spl2));
+
+
+// 4
+let ar = [1, 2, 3, 4, 5];
+
+let sum = ar.reduce((acc, elem) => {
+    return acc + elem 
+}, 0)
+
+console.log(sum);
+
+
+// 5
+let ten=[2,15,10,24]
+
+let  unten = ten.filter((elem) => elem !== 10)
+
+console.log(unten);
+
+
+// 6
+let div = [12, 25, 3, 6, 8, 14, 7, 23];
+
+let div3 = div.map((element) => element / 3)
+
+console.log(div3);
+
+
+// 9
+let arr1 = [1, 2];
+let arr2 = [3, 4];
+let arr3 = [5, 6];
+
+console.log(arr1.concat(arr2, arr3))
+
+
+// 10
+let arr = [-1, -2, -3, 4];
+
+let zero = arr.some((moreThanZero) => moreThanZero > 0)
+
+console.log(zero);
+
+
+// 11
+let sort =[23,45,32,5,87,7,3,98]
+
+sort.sort((a, b) => b - a)
+sort.pop()
+
+console.log(sort);
